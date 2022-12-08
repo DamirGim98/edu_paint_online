c4a1de1f7541d9e3d414e5b029cff4f28c0d7399
Damir Gimatdinov <damirgim@yandex.ru>
2022-12-08 10:19:54 +0400

    Fixes slider background color

--- src/feauture/canvas/UI/WidthSlider.jsx
+++ src/feauture/canvas/UI/WidthSlider.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { InputNumber, Slider } from 'antd'
+import { Slider } from 'antd'
 import Controller from '../Controller'
 import '../../../styles/slider.scss'
 
@@ -18,17 +18,7 @@ const WidthSlider = () => {
         onChange={onChange}
         value={typeof inputValue === 'number' ? inputValue : 0}
       />
-      <InputNumber
-        readOnly
-        className="slider__number"
-        min={1}
-        max={40}
-        style={{
-          margin: '0 16px',
-        }}
-        value={inputValue}
-        onChange={onChange}
-      />
+      <div className="slider__number">{inputValue}</div>
     </div>
   )
 }
